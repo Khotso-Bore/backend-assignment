4c6f5e696ef76cb7f528b5b23e09d07a0673e05a
Khotso <82458587+Khotso-Bore@users.noreply.github.com>
2024-10-06 16:36:39 +0200

    added rabbitmq settings class

---
+++ src/OT.Assessment.Infrastructure/Settings/RabbitSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OT.Assessment.Infrastructure.Settings
+{
+    public class RabbitSettings
+    {
+        public string User {  get; set; }
+        public string Password { get; set; }
+        public string HostName { get; set; }
+        public int Port { get; set; }
+        public string ExchangeName { get; set; }
+        public string QueueName {  get; set; }
+        public string RoutingKey { get; set; }
+    }
+}
