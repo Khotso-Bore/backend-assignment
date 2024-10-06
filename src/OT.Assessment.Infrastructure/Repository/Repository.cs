2d7b1865466bb0da82652440177d03c5678116c4
Khotso <82458587+Khotso-Bore@users.noreply.github.com>
2024-10-06 16:26:29 +0200

    unit of work and repository base members

---
+++ src/OT.Assessment.Infrastructure/Repository/Repository.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OT.Assessment.Domain.Data;
+using OT.Assessment.Infrastructure.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OT.Assessment.Infrastructure.Repository
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private OTDbContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+        public Repository(OTDbContext context) {
+
+            _context = context;
+            _dbSet = context.Set<TEntity>();
+        }
+
+        public IQueryable<TEntity> GetSet()
+        {
+           return _dbSet.AsQueryable();
+        }
+
+        public async Task<List<TEntity>> Where(Expression<Func<TEntity, bool>> expression)
+        {
+            return await _dbSet.Where(expression).ToListAsync();
+        }
+
+        public async Task<TEntity> FindOne(Expression<Func<TEntity, bool>> expression)
+        {
+            var response = await _dbSet.FirstOrDefaultAsync(expression);
+            if (response != null)
+                return null;
+
+            return response;
+        }
+
+        public async Task Update(Expression<Func<TEntity, bool>> expression, TEntity entity)
+        {
+            var response = await _dbSet.FirstOrDefaultAsync(expression);
+            if (response != null)
+                return;
+
+            _dbSet.Update(entity);
+            return;
+        }
+
+        public async Task DeleteOne(Expression<Func<TEntity, bool>> expression)
+        {
+            var response = await _dbSet.FirstOrDefaultAsync(expression);
+            if (response != null)
+                return;
+
+            _dbSet.Remove(response);
+            return;
+        }
+
+        public async Task<TEntity> InsertOne(TEntity entity)
+        {
+            var response = await _dbSet.AddAsync(entity);
+            return entity;
+
+        }
+    }
+}
---
+++ src/OT.Assessment.Infrastructure/Repository/UnitOfWork.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OT.Assessment.Domain.Data;
+using OT.Assessment.Domain.Entities;
+using OT.Assessment.Infrastructure.IRepository;
+using OT.Assessment.Tester.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OT.Assessment.Infrastructure.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly OTDbContext _context;
+
+        public IRepository<Player> Players { get; private set; }
+        public IRepository<Provider> Providers { get; private set; }
+        public IRepository<CasinoWager> Wagers{ get; private set; }
+        public IRepository<Game> Games { get; private set; }
+
+        public UnitOfWork(OTDbContext context)
+        {
+            _context = context;
+            Players = new Repository<Player>(context);
+            Providers = new Repository<Provider>(context);
+            Wagers = new Repository<CasinoWager>(context);
+            Games = new Repository<Game>(context);
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+
+
+
+    }
+}
