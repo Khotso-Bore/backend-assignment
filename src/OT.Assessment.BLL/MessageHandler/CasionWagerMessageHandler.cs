4e81c4aeb45f5287ef9f3a9da8df880958346666
Khotso <82458587+Khotso-Bore@users.noreply.github.com>
2024-10-06 16:34:57 +0200

    created rabbit messageHandler interface and base

---
+++ src/OT.Assessment.BLL/MessageHandler/CasionWagerMessageHandler.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using OT.Assessment.Infrastructure.DTO;
+using OT.Assessment.Infrastructure.IRepository;
+using OT.Assessment.Infrastructure.MessageHandler;
+using OT.Assessment.Tester.Infrastructure;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace OT.Assessment.BLL.MessageHandler
+{
+    public class CasionWagerMessageHandler : IMessageHandler
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public CasionWagerMessageHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async void HandleMessage(byte[] message)
+        {
+            if (message.Length == 0)
+                return;
+
+            var addCasinoWagerDTO = JsonSerializer.Deserialize<AddCasionWagerDTO>(message);
+
+            if (addCasinoWagerDTO.Duration < 0)
+                return ;
+
+            if (addCasinoWagerDTO.NumberOfBets < 1)
+                return;
+
+            var player = await _unitOfWork.Players.FindOne(x => x.Id.Equals(addCasinoWagerDTO.AccountId));
+            if (player == null)
+                return ;
+
+            var provider = await _unitOfWork.Providers.FindOne(x => x.Name.Equals(addCasinoWagerDTO.Provider));
+            if (provider == null)
+                return;
+
+            var games = await _unitOfWork.Games.FindOne(x => x.Name.Equals(addCasinoWagerDTO.GameName) && x.ProviderId.Equals(provider.Id));
+            if (games == null)
+                return;
+
+            var casionWager = _mapper.Map<CasinoWager>(addCasinoWagerDTO);
+
+            await _unitOfWork.Wagers.InsertOne(casionWager);
+
+            _unitOfWork.Commit();
+
+        }
+    }
+}
---
+++ src/OT.Assessment.Infrastructure/MessageHandler/IMessageHandler.cs
@@ -0,0 +1,14 @@
+﻿using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OT.Assessment.Infrastructure.MessageHandler
+{
+    public interface IMessageHandler
+    {
+        void HandleMessage(byte[] bytes);
+    }
+}
