0e68a3b4961e918c7933a461a0fc94d000e8a77b
Khotso <82458587+Khotso-Bore@users.noreply.github.com>
2024-10-06 16:23:55 +0200

    added database context

---
+++ src/OT.Assessment.Domain/Data/OTDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OT.Assessment.Domain.Entities;
+using OT.Assessment.Tester.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OT.Assessment.Domain.Data
+{
+    public class OTDbContext : DbContext
+    {
+        public OTDbContext(DbContextOptions<OTDbContext> options) : base(options) { }
+
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Provider> Providers { get; set; }
+        public DbSet<Game> Games { get; set; }
+        public DbSet<CasinoWager> CasinoWagers { get; set; }
+    }
+}
