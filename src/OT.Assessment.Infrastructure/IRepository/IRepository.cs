f232df8929fbefff9d97113777c75c639a4e610a
Khotso <82458587+Khotso-Bore@users.noreply.github.com>
2024-10-06 16:26:00 +0200

    added unit of work and repository interfaces

---
+++ src/OT.Assessment.Infrastructure/IRepository/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OT.Assessment.Infrastructure.IRepository
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        IQueryable<TEntity> GetSet();
+        Task<List<TEntity>> Where(Expression<Func<TEntity, bool>> expression);
+        Task<TEntity> FindOne(Expression<Func<TEntity, bool>> expression);
+        Task Update(Expression<Func<TEntity, bool>> expression, TEntity entity);
+        Task DeleteOne(Expression<Func<TEntity, bool>> expression);
+        Task<TEntity> InsertOne(TEntity entity);
+
+    }
+}
---
+++ src/OT.Assessment.Infrastructure/IRepository/IUnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using OT.Assessment.Domain.Entities;
+using OT.Assessment.Tester.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OT.Assessment.Infrastructure.IRepository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<Player> Players { get; }
+        IRepository<Provider> Providers { get; }
+        IRepository<Game> Games { get; }
+        IRepository<CasinoWager> Wagers { get; }
+
+        void Commit();
+        void Dispose();
+    }
+}
