0809be7f8ffa09c94a8f6894acbb2247759346b3
Khotso <82458587+Khotso-Bore@users.noreply.github.com>
2024-10-06 16:28:17 +0200

    DTOs for player controller

---
+++ src/OT.Assessment.Infrastructure/DTO/PlayerWagersDTO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OT.Assessment.Infrastructure.DTO
+{
+    public class PlayerWagersDTO
+    {
+        public List<WagerDTO> Data { get; set; }
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+        public int Total { get; set; }
+        public int TotalPages { get; set; }
+        
+    }
+
+    public class WagerDTO
+    {
+        public Guid WagerId { get; set; }
+        public string Game { get; set; }
+        public string Provider { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+
+
+}
---
+++ src/OT.Assessment.Infrastructure/DTO/SpenderDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OT.Assessment.Infrastructure.DTO
+{
+    public class SpenderDTO
+    {
+        public Guid AccountId { get; set; }
+        public string Username { get; set; }
+        public decimal TotalAmountSpend { get; set; }
+    }
+}
