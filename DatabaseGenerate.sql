ea6ce5cd8cd28c18a1467011e08874f48e3986bc
Khotso <82458587+Khotso-Bore@users.noreply.github.com>
2024-10-07 00:35:03 +0200

    removed unneccesary tables

--- DatabaseGenerate.sql
+++ DatabaseGenerate.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Players (
+/*CREATE TABLE Players (
 	Id uniqueidentifier PRIMARY KEY,
 	Username varchar(50),
 	Constraint AK_Username UNIQUE(Username)
@@ -16,23 +16,26 @@ CREATE TABLE Games(
 	Name varchar(50),
 	Theme varchar(50),
 	CONTRAINT FK_ProviderId FOREIGN KEY (ProviderId) REFERENCES Providers(Id),
-);
+);*/
 
 CREATE TABLE CasinoWagers (
-	Id uniqueidentifier PRIMARY KEY,
+	WagerId uniqueidentifier PRIMARY KEY,
+	Theme varchar(50),
+	Provider varchar(50),
+	GameName varchar(50),
+	TransactionId uniqueidentifier,
+	BrandId uniqueidentifier,
 	AccountId uniqueidentifier,
-	GameId uniqueidentifier,
-	NumberOfBets int,
-	CountryCode char(2),
-	Amount DECIMAL(30,12),
-	Duration INT,
-	SesstionData NVARCHAR,
+	Username varchar(50),
 	ExternalReferenceId uniqueidentifier,
 	TransactionTypeId uniqueidentifier,
-	TransactionId uniqueidentifier,
-	CreatedDate DATE,
-	CONTRAINT FK_AccountId FOREIGN KEY (AccountId) REFERENCES Players(Id),
-	CONTRAINT FK_GameId FOREIGN KEY (GameId) REFERENCES Games(Id),
+	Amount DECIMAL(30,12),
+	CreatedDateTime DATE,
+	NumberOfBets INT,
+	CountryCode char(2),
+	SessionData VARCHAR(MAX),
+	Duration INT,
+	INDEX IX_AccountId (AccountId),
 );
 
 
