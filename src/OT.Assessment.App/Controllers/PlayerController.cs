0e9b20bbd9bd500aedba7bbe2a2829a9f4ac9b1d
Khotso <82458587+Khotso-Bore@users.noreply.github.com>
2024-10-07 00:26:37 +0200

    made use of a lock and sattic queeue for batch publishing

--- src/OT.Assessment.App/Controllers/PlayerController.cs
+++ src/OT.Assessment.App/Controllers/PlayerController.cs
@@ -9,6 +9,10 @@ namespace OT.Assessment.App.Controllers
     public class PlayerController : ControllerBase
     {
         private readonly IPlayersService _playerService;
+        private int batchSize = 500;
+        private Object thisLock = new Object();
+        private static Queue<AddCasionWagerDTO> _casionWagerDTOs = new Queue<AddCasionWagerDTO> ();
+        //private readonly Lock _balanceLock = new();
 
         public PlayerController(IPlayersService playerService)
         {
@@ -16,15 +20,36 @@ namespace OT.Assessment.App.Controllers
         }
 
         //POST api/player/casinowager
-        [HttpPost("casinowager")]
+        [HttpPost("CasinoWager")]
         public async Task<IActionResult> AddCasinoWager(AddCasionWagerDTO addCasionWagerDTO)
         {
-          _playerService.AddCasinoWager(addCasionWagerDTO);
-          return Ok();
+
+            _casionWagerDTOs.Enqueue(addCasionWagerDTO);
+
+            lock (thisLock)
+            {
+                if (_casionWagerDTOs.Count >= batchSize)
+                {
+
+                    List<AddCasionWagerDTO> addCasionWagerDTOs = new List<AddCasionWagerDTO>();
+                    for (int i = 0; i < batchSize; i++)
+                    {
+                        addCasionWagerDTOs.Add(_casionWagerDTOs.Dequeue());
+
+                    }
+
+                    _playerService.AddBulk(addCasionWagerDTOs);
+                }
+            }
+
+
+            return Ok();
         }
 
+
+
         //GET api/player/{playerId}/wagers
-        [HttpGet("{playerId}/wagers")]
+        [HttpGet("{playerId}/Wagers")]
         public async Task<IActionResult> AddCasinoWager(string playerId,int pageSize,int page)
         {
             if(page < 1)
@@ -37,7 +62,7 @@ namespace OT.Assessment.App.Controllers
         }
 
         //GET api/player/topSpenders?count=10
-        [HttpGet("topSpenders")]
+        [HttpGet("TopSpenders")]
         public async Task<IActionResult> TopSpenders(int count)
         {
             if (count < 1)
