4612f571d7e6ca540085ff0f1afdfce425abc9e6
Khotso <82458587+Khotso-Bore@users.noreply.github.com>
2024-10-06 16:31:13 +0200

    added Automapper and mapping profile

---
+++ src/OT.Assessment.Infrastructure/AutoMapper/MapperProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using OT.Assessment.Infrastructure.DTO;
+using OT.Assessment.Tester.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OT.Assessment.Infrastructure.AutoMapper
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<AddCasionWagerDTO, CasinoWager>().
+                ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.WagerId));
+        }
+    }
+}
